2657051a24cd73f9370accc3e414b61258760d59
Vladimir Roncevic <elektron.ronca@gmail.com>
2017-06-08 23:33:01 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# build_step
+Build step tool for x86-64 and GCC4.9.2 (Bash script).
+================================================================================
+
+The README is used to introduce the tool and provide instructions on
+how to install the tool, any machine dependencies it may have and any
+other information that should be provided before the tool is installed.
+
+INSTALLATION
+
+To install this tool type the following:
+
+	cp -R ~/build_step/bin/   /root/scripts/build_step/ver.1.0/
+
+	cp -R ~/build_step/conf/  /root/scripts/build_step/ver.1.0/
+
+	cp -R ~/build_step/log/   /root/scripts/build_step/ver.1.0/
+
+
+DEPENDENCIES
+
+This tool requires these other modules and libraries:
+
+	sh_util https://github.com/vroncevic/sh_util
+
+COPYRIGHT AND LICENCE
+
+Copyright (C) 2017 by https://github.com/vroncevic/build_step
+
+This tool is free software; you can redistribute it and/or modify
+it under the same terms as Bash itself, either Bash version 4.2.47 or,
+at your option, any later version of Bash 4 you may have available.
+
+![alt tag](https://raw.githubusercontent.com/vroncevic/build_step/master/bash_logo.png)
+![alt tag](https://raw.githubusercontent.com/vroncevic/build_step/master/linux_logo.jpg)
+
---
+++ bin/build_step.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#
+# @brief   build_step build native App/Tool in steps
+# @version ver.1.0
+# @date    Wed Jun  7 22:25:23 CEST 2017
+# @company None, free software to use 2017
+# @author  Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+UTIL_ROOT=/root/scripts
+UTIL_VERSION=ver.1.0
+UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
+UTIL_LOG=${UTIL}/log
+
+.	${UTIL}/bin/devel.sh
+.	${UTIL}/bin/usage.sh
+.	${UTIL}/bin/check_root.sh
+.	${UTIL}/bin/check_tool.sh
+.	${UTIL}/bin/logging.sh
+.	${UTIL}/bin/send_mail.sh
+.	${UTIL}/bin/load_conf.sh
+.	${UTIL}/bin/load_util_conf.sh
+.	${UTIL}/bin/progress_bar.sh
+
+BUILD_STEP_TOOL=build_step
+BUILD_STEP_VERSION=ver.1.0
+BUILD_STEP_HOME=${UTIL_ROOT}/${BUILD_STEP_TOOL}/${BUILD_STEP_VERSION}
+BUILD_STEP_CFG=${BUILD_STEP_HOME}/conf/${BUILD_STEP_TOOL}.cfg
+BUILD_STEP_UTIL_CFG=${BUILD_STEP_HOME}/conf/${BUILD_STEP_TOOL}_util.cfg
+BUILD_STEP_LOG=${BUILD_STEP_HOME}/log
+
+declare -A BUILD_STEP_USAGE=(
+	[USAGE_TOOL]="__${BUILD_STEP_TOOL}"
+	[USAGE_ARG1]="[SOURCE_FILE] Source code file name"
+	[USAGE_ARG2]="[OUTPUT_FILE] Output file name"
+	[USAGE_EX_PRE]="# Example build steps for main.c"
+	[USAGE_EX]="__${BUILD_STEP_TOOL} main.c hello_world"
+)
+
+declare -A BUILD_STEP_LOGGING=(
+	[LOG_TOOL]="${BUILD_STEP_TOOL}"
+	[LOG_FLAG]="info"
+	[LOG_PATH]="${BUILD_STEP_LOG}"
+	[LOG_MSGE]="None"
+)
+
+declare -A PB_STRUCTURE=(
+	[BW]=50
+	[MP]=100
+	[SLEEP]=0.01
+)
+
+TOOL_DEBUG="false"
+TOOL_LOG="false"
+TOOL_NOTIFY="false"
+
+#
+# @brief  build_step build native App/Tool in steps
+# @params Values required source code file name and output file name
+# @retval Function __build_step exit with integer value
+#			0   - tool finished with success operation
+#			128 - missing argument(s) from cli
+#			129 - failed to load tool script configuration from files
+#			130 - check input source code file
+#
+# @usage
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# CF="main.c" OF="hello_world"
+# __build_step "$CF" "$OF"
+#
+function __build_step() {
+	local CF=$1 OF=$2
+	if [[ -n "${CF}" && -n "${OF}" ]]; then
+		local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL STATUS
+		MSG="Loading basic and util configuration!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		__progress_bar PB_STRUCTURE
+		declare -A config_build_step=()
+		__load_conf "$BUILD_STEP_CFG" config_build_step
+		STATUS_CONF=$?
+		declare -A config_build_step_util=()
+		__load_util_conf "$BUILD_STEP_UTIL_CFG" config_build_step_util
+		STATUS_CONF_UTIL=$?
+		declare -A STATUS_STRUCTURE=(
+			[1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
+		)
+		__check_status STATUS_STRUCTURE
+		STATUS=$?
+		if [ $STATUS -eq $NOT_SUCCESS ]; then
+			MSG="Force exit!"
+			__info_debug_message_end "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+			exit 129
+		fi
+		TOOL_DEBUG=${config_build_step[DEBUGGING]}
+		TOOL_LOG=${config_build_step[LOGGING]}
+		TOOL_NOTIFY=${config_build_step[EMAILING]}
+		if [ ! -f "${CF}" ]; then
+			MSG="Check file [${CF}]"
+			__info_debug_message_end "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+			exit 130
+		fi
+		MSG="C preprocessor!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		local FILENAME="${CF%.*}"
+		cpp ${CF} > ${FILENAME}.i
+		MSG="Generating assembly code!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		cc -S ${FILENAME}.i
+		MSG="Generating object file!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		as -o ${FILENAME}.o ${FILENAME}.s
+		MSG="Generating ELF file!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		local LL C1 CI CB IN CE CN ARGS1 ARGS2 ARGS3
+		LL=${config_build_step_util[LD_LINUX]}
+		C1=${config_build_step_util[CRT1]}
+		CI=${config_build_step_util[CRTI]}
+		CB=${config_build_step_util[CRT_BEGIN]}
+		IN=${config_build_step_util[INCLUDE]}
+		CE=${config_build_step_util[CRT_END]}
+		CN=${config_build_step_util[CRTN]}
+		ARGS1="-dynamic-linker ${LL}"
+		ARGS2="${C1} ${CI} ${CB}"
+		ARGS3="-L ${IN} -lc ${CE} ${CN}"
+		eval "ld ${ARGS1} -o ${OF} $ARGS2 ${FILENAME}.o ${ARGS3}"
+		MSG="Set owner!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		local USRID=${config_build_step_util[UID]}
+		local GRPID=${config_build_step_util[GID]}
+		eval "chown ${USRID}.${GRPID} ${FILENAME}.*"
+		eval "chown ${USRID}.${GRPID} ${OF}"
+		MSG="Set permission!"
+		__info_debug_message "$MSG" "$FUNC" "$BUILD_STEP_TOOL"
+		eval "chmod 644 ${FILENAME}.*"
+		eval "chmod 755 ${OF}"
+		__info_debug_message_end "Done" "$FUNC" "$BUILD_STEP_TOOL"
+		exit 0
+	fi
+	__usage BUILD_STEP_USAGE
+	exit 128
+}
+
+#
+# @brief   Main entry point of script tool
+# @params  Values required source code file name and output file name
+# @exitval Script tool build_step exit with integer value
+#			0   - tool finished with success operation
+#			127 - run tool script as root user from cli
+#			128 - missing argument(s) from cli
+#			129 - failed to load tool script configuration from files
+#			130 - check input source code file
+#
+printf "\n%s\n%s\n\n" "${BUILD_STEP_TOOL} ${BUILD_STEP_VERSION}" "`date`"
+__check_root
+STATUS=$?
+if [ $STATUS -eq $SUCCESS ]; then
+	__build_step $1 $2
+fi
+
+exit 127
+
