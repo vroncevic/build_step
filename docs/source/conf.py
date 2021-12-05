04b00671608d59b1c260331377c0c77b45e9d905
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-12-05 12:09:54 +0100

    [build_step] updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 project = u'build_step'
 copyright = u'2017, Vladimir Roncevic <elektron.ronca@gmail.com>'
 author = u'Vladimir Roncevic <elektron.ronca@gmail.com>'
-version = u'1.0'
+version = u'2.0'
 release = u'https://github.com/vroncevic/build_step/releases'
 extensions = []
 templates_path = ['_templates']
